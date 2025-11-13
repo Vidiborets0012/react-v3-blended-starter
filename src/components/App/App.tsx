cf3fc521610f5a4596d8cfc6543976699d84d4b1
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-13 11:54:48 +0200

    Initialize the project, type the section and container components, Photo interface, process the Form component.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
 import Section from "../Section/Section";
 import Container from "../Container/Container";
+import Form from "../Form/Form";
 
 export default function App() {
+  const handleSearch = (searchQuery: string) => {
+    console.log("searchQuery:", searchQuery);
+  };
   return (
     <>
       <Section>
-        <Container>{/* Компоненти застосунку */}</Container>
+        <Container>{<Form onSubmit={handleSearch} />}</Container>
         Home page
       </Section>
     </>
--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,5 +1,11 @@
+// import type { ReactNode } from "react";
+import type React from "react";
 import style from "./Section.module.css";
 
-export default function Section({ children }) {
+interface SectionProps {
+  children: React.ReactNode;
+}
+
+export default function Section({ children }: SectionProps) {
   return <section className={style.section}>{children}</section>;
 }
