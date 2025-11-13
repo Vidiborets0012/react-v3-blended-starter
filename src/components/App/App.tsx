a93ab046fcce863338bd1979760fa0210b824c3c
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-11-13 16:40:42 +0200

    Implement data retrieval

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,25 @@
 import Section from "../Section/Section";
 import Container from "../Container/Container";
 import Form from "../Form/Form";
+import { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import type { Photo } from "../../types/photo";
+import { getPhotos } from "../../services/photos";
 
 export default function App() {
-  const handleSearch = (searchQuery: string) => {
-    console.log("searchQuery:", searchQuery);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  const handleSearch = async (searchQuery: string) => {
+    try {
+      const fetchedPhotos = await getPhotos(searchQuery);
+      setPhotos(fetchedPhotos);
+      // console.log("fetchedPhotos:", fetchedPhotos);
+      // console.log("searchQuery:", searchQuery);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log("lalala");
+    }
   };
   return (
     <>
@@ -12,6 +27,7 @@ export default function App() {
         <Container>{<Form onSubmit={handleSearch} />}</Container>
         Home page
       </Section>
+      <Toaster position="top-right" />
     </>
   );
 }
