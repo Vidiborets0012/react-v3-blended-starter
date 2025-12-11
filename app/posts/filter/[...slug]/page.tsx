d9ed5f5bd8609f8232fcd411dfcbac03993b9870
Vidiborets Viktor <vidiboretsv@gmail.com>
2025-12-11 20:57:25 +0200

    Implement the posts/filter/[userId] page

---
+++ app/posts/filter/[...slug]/page.tsx
@@ -0,0 +1,23 @@
+// import { fetchPosts } from '@/lib/api';
+
+import { fetchPosts } from '@/lib/api';
+import PostsClient from './Posts.client';
+
+interface PostsPageProps {
+  params: Promise<{ slug: string[] }>;
+}
+
+export default async function PostsPage({ params }: PostsPageProps) {
+  const { slug } = await params;
+  // console.log('slug:', slug);
+
+  const userId = slug[0];
+
+  const data = await fetchPosts({
+    searchText: '',
+    page: 1,
+    ...(userId && userId !== 'all' && { userId }),
+  });
+
+  return <PostsClient initialData={data} userId={userId} />;
+}
